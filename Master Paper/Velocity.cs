839014cad494593330e23f6b67854577f3a3d35a
Vitaliy_N_1997 <vitaliy_n_1997@ukr.net>
2019-11-29 11:15:50 +0200

    The Velocity class was added

---
+++ Master Paper/Velocity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Master_Paper
+{
+    //Клас для швидкості
+    static class Velocity
+    {
+        //Обчислення mu
+        public static double getMu(double h1, double V, double nu)
+        {
+            return 1 / (1 + h1 * V / nu);
+        }
+
+        //Обчислення rPlus
+        public static double getRPlus(double V)
+        {
+            return (V + Abs(V)) / 2;
+        }
+
+        //Обчислення rMinus
+        public static double getRMinus(double V)
+        {
+            return (V - Abs(V)) / 2;
+        }
+
+        //Обчислення тиску
+        public static double getPressure(double ro, double gamma, double l, double Z1, double Z2)
+        {
+            return gamma * (Z1 - Z2) / (ro * l);
+        }
+
+        //Обчислення f
+        public static double getF(double V, double tau, double pressure)
+        {
+            return V / (2 * tau) - pressure;
+        }
+    }
+}
