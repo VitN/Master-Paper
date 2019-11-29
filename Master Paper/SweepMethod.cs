0a8fdc330aac4fa4a62797a8bad6f8d144beea1c
Vitaliy_N_1997 <vitaliy_n_1997@ukr.net>
2019-11-29 10:09:52 +0200

    The class SweepMethod was made static. And some new methods were added

--- Master Paper/SweepMethod.cs
+++ Master Paper/SweepMethod.cs
@@ -8,40 +8,61 @@ using static System.Math;
 namespace Master_Paper
 {
     //Клас для методу прогонки
-    class SweepMethod
+   static class SweepMethod
     {
         //Обчислення коефіцієнта а
-        public double getA(double mu, double rMinus, double h1)
+        //для поздовжньої прогонки
+        public static double getA(double mu, double rMinus, double h1)
         {
             return Pow(mu, 2) / Pow(h1, 2) - Pow(rMinus, 2) / h1;
         }
+        
+        //для поперечної прогонки
+        public static double getA(double h2)
+        {
+            return 1 / Pow(h2, 2);
+        }
 
         //Обчислення коефіцієнта b
-        public double getB(double mu, double rPlus, double h1)
+        //для поздовжньої прогонки
+        public static double getB(double mu, double rPlus, double h1)
         {
             return Pow(mu, 2) / Pow(h1, 2) + Pow(rPlus, 2) / h1;
         }
 
-        //Обчислення коефіцієнта b
-        public double getC(double mu, double rPlus, double rMinus, double Addition, double h1)
+        //для поперечної прогонки
+        public static double getB(double h2)
         {
-            return 2 * Pow(mu, 2) / Pow(h1, 2) + (Pow(rPlus, 2) - Pow(rMinus, 2)) / h1;
+            return 1 / Pow(h2, 2);
+        }
+
+        //Обчислення коефіцієнта c
+        //для поздовжньої прогонки
+        public static double getC(double mu, double rPlus, double rMinus, double Addition, double h1)
+        {
+            return 2 * Pow(mu, 2) / Pow(h1, 2) + (Pow(rPlus, 2) - Pow(rMinus, 2)) / h1 + Addition;
+        }
+
+        //для поперечної прогонки
+        public static double getC(double h2, double gamma, double D, double tau)
+        {
+            return 2 / Pow(h2, 2) + 0.5 * gamma / D + 0.5 / (D * tau);
         }
 
         //Обчислення коефіцієнта альфа 
-        public double getAlpha(double a, double b, double c, double alphaPrev)
+        public static double getAlpha(double a, double b, double c, double alphaPrev)
         {
             return b / (c - alphaPrev / a);
         }
 
         //Обчислення коефіцієнта бета
-        public double getBetha(double a, double b, double c, double f, double alphaPrev, double bethaPrev)
+        public static double getBetha(double a, double b, double c, double f, double alphaPrev, double bethaPrev)
         {
             return  (a * bethaPrev + f)/ (c * c - alphaPrev * alphaPrev / (a * a));
         }
 
         //Обчислення значення концентрації чи швидкості
-        public double getValue(double NextValue, double alpha, double betha)
+        public static double getValue(double NextValue, double alpha, double betha)
         {
             return alpha * NextValue + betha;
         }
